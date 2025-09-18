1767f863b34c537c156874892dbe4a7a27712c1d
isaiahgalaviz07 <isaiahgalaviz07@gmail.com>
2025-09-18 16:37:36 -0400

    Finished Application (for now...)

--- base/views.py
+++ base/views.py
@@ -3,11 +3,9 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic, Message
-from .forms import RoomForm, UserForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # 
 def home(request):
@@ -143,15 +141,15 @@ def loginPage(request):
         return redirect("Home")
 
     if request.method == "POST":
-        username = request.POST.get("Username").lower()
+        email = request.POST.get("Email").lower()
         password = request.POST.get("Password")
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, "User does not exist.")
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         
         if user is not None:
             login(request, user)
@@ -171,11 +169,11 @@ def logoutUser(request):
 #
 def registerPage(request):
     page = "Register"
-    form = UserCreationForm()
+    form = MyUserCreationForm()
     context = {"page": page, "form": form}
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -183,7 +181,7 @@ def registerPage(request):
             login(request, user)
             return redirect("Home")
         else:
-            messages.error(request, "An ERROR occured during registration!")
+            messages.error(request, "Password is either too short,\n or the name or email is already taken!")
 
     return render(request, "base/login_register.html", context)
 
@@ -194,7 +192,7 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == "POST":
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect("User Profile", pk=user.id)
--- studybud/urls.py
+++ studybud/urls.py
@@ -1,7 +1,8 @@
 # Utilize the following imports
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),        #  URL for the admin page
@@ -9,3 +10,4 @@ urlpatterns = [
     path('api/', include('base.api.urls'))  #  URLS from the app's API
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
